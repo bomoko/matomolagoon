6e37e0cbfd14c1b89d98b7326075c31cbd491f40
Blaize Kaye <blaize.kaye@amazee.io>
2025-08-14 07:53:10 +1200

    chown on startup

--- .lagoon/entrypoints/copy_seed_files.sh
+++ .lagoon/entrypoints/copy_seed_files.sh
@@ -3,3 +3,7 @@
 if [ -d /app/config ] && [ ! "$(ls -A /app/config)" ]; then
     cp -a /seedfiles/config/. /app/config/
 fi
+
+
+# now we fix permissions
+chown -R 1000:1000 /app/config /app/plugins /app/tmp /app/plugins_persistent
